5d62acd51db19f51fb720b74f0b637246035f0a5
Manuel Álvarez Rosado <manuelalvarezrosado@MacBook-Air-de-Manuel.local>
2019-11-15 00:52:58 +0100

    Que cojones ha pasado? que lo estaba terminando y se me reiniciaba cada x tiempo el archivo style.css y se me borraba todo

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,25 @@
 const express = require('express');
 const router  = express.Router();
+const Movie = require('../models/Movie.js');
+
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+router.get('/movies', (req, res, next) => {
+  Movie.find({})
+    .then(document => {
+      const movies = document;
+      // res.send(movies);
+      res.render('movies', { movies });
+    })
+    .catch(err => {
+      console.log(err);
+      next();
+    });
+});
+
 
 module.exports = router;
